f788e7180b4d9368a087897e4cd31d7bc13ace61
MocanuAlinD <alin_ngt@yahoo.com>
2021-04-09 11:41:32 +0300

    Imagini mici si mare OK

--- components/ListImages.js
+++ components/ListImages.js
@@ -2,18 +2,22 @@ import styles from '../styles/NavbarPages.module.css'
 import Image from 'next/image'
 
 const ListImages = (props) => {
-    const {img} = props
+    const { img } = props
     // console.log('Img is: ',img)
+    const moc = null
+    const alin = () => {
+        console.log('alin')
+    }
     return (
         <div className={styles.inside}>
-            {img.map(a => 
+            {img.map(a =>
                 <div>
-                    <Image key={a.id} className={styles.imageLink} src={a.url} width={a.width} height={a.height} />
+                    <Image onClick={alin} key={a.id} className={styles.imageLink} src={a.url} width={a.width} height={a.height} />
                 </div>
             )}
-           
+
         </div>
     );
 }
- 
+
 export default ListImages;
