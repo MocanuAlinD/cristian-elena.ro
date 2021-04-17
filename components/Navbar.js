52e6ae75587d79ef16d2801c479e6c060dd53f97
MocanuAlinD <alin_ngt@yahoo.com>
2021-04-17 14:10:47 +0300

    MainPage burger menu OK

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,53 +1,119 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Burger from './Burger'
 
-const Navbar = () => {
-    return (
-        <div className={styles.entireMenu}>
-            <div className={styles.title}>
-                <h1>Cristian Elena</h1>
+
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    constructor(props){
+        super(props)
+        this.state = { active: true }
+    }
+    changeActive(item) {
+        this.setState({ active: item })
+    }
+    render() {
+        return (
+            <div className={styles.entireMenu}>
+                <div className={styles.title}>
+                    <h1>Cristian Elena</h1>
+                </div>
+                <div className={styles.brandTitle}>
+                    <h4>cristian-elena.ro</h4>
+                </div>
+                <div className={styles.toggleButton}>
+                    <div className={styles.line1}></div>
+                    <div className={styles.line2}></div>
+                    <div className={styles.line3}></div>
+                </div>
+                <div>
+                    <ul className={this.state.active ? styles.menuListActive : styles.menuListInactive}>
+                        <li>
+                            <Link href='./portraits'><a>portraits</a></Link>
+                        </li>
+                        <li>
+                            <Link href='./kids'><a>kids</a></Link>
+                        </li>
+                        <li>
+                            <Link href='./preborn'><a>preborn</a></Link>
+                        </li>
+                        <li>
+                            <Link href='./newborn'><a>newborn</a></Link>
+                        </li>
+                        <li>
+                            <Link href='./food'><a>food</a></Link>
+                        </li>
+                        <li>
+                            <Link href='./city'><a>city</a></Link>
+                        </li>
+                        <li>
+                            <Link href='./landscape'><a>landscape</a></Link>
+                        </li>
+                    </ul>
+                    <Burger data={{ active: this.state.active, changeActive: this.changeActive.bind(this) }}/>
+                </div>
             </div>
-            <div className={styles.brandTitle}>
-                <h4>cristian-elena.ro</h4>
-            </div>
-            <div className={styles.toggleButton}>
-                <div className={styles.line1}></div>
-                <div className={styles.line2}></div>
-                <div className={styles.line3}></div>
-            </div>
-            <div>
-                <ul className={styles.menuList}>
-                    <li>
-                        <Link href='./portraits'><a>portraits</a></Link>
-                    </li>
-                    <li>
-                        <Link href='./kids'><a>kids</a></Link>
-                    </li>
-                    <li>
-                        <Link href='./preborn'><a>preborn</a></Link>
-                    </li>
-                    <li>
-                        <Link href='./newborn'><a>newborn</a></Link>
-                    </li>
-                    <li>
-                        <Link href='./food'><a>food</a></Link>
-                    </li>
-                    <li>
-                        <Link href='./city'><a>city</a></Link>
-                    </li>
-                    <li>
-                        <Link href='./landscape'><a>landscape</a></Link>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
+        );
+    }
 }
- 
+
 export default Navbar;
 
-/*
-ul-li - menuList
 
 
-*/
+
+
+
+
+
+
+
+
+
+
+// const Navbar = () => {
+//     return (
+//         <div className={styles.entireMenu}>
+//             <div className={styles.title}>
+//                 <h1>Cristian Elena</h1>
+//             </div>
+//             <div className={styles.brandTitle}>
+//                 <h4>cristian-elena.ro</h4>
+//             </div>
+//             <div className={styles.toggleButton}>
+//                 <div className={styles.line1}></div>
+//                 <div className={styles.line2}></div>
+//                 <div className={styles.line3}></div>
+//             </div>
+//             <div>
+//                 <ul className={styles.menuList}>
+//                     <li>
+//                         <Link href='./portraits'><a>portraits</a></Link>
+//                     </li>
+//                     <li>
+//                         <Link href='./kids'><a>kids</a></Link>
+//                     </li>
+//                     <li>
+//                         <Link href='./preborn'><a>preborn</a></Link>
+//                     </li>
+//                     <li>
+//                         <Link href='./newborn'><a>newborn</a></Link>
+//                     </li>
+//                     <li>
+//                         <Link href='./food'><a>food</a></Link>
+//                     </li>
+//                     <li>
+//                         <Link href='./city'><a>city</a></Link>
+//                     </li>
+//                     <li>
+//                         <Link href='./landscape'><a>landscape</a></Link>
+//                     </li>
+//                 </ul>
+//                 <Burger />
+//             </div>
+//         </div>
+//     );
+// }
+ 
+// export default Navbar;
--- components/NavbarPages.js
+++ components/NavbarPages.js
@@ -3,16 +3,17 @@ import Link from 'next/link'
 
 const NavbarPages = (props) => {
     return (
-        <div className={styles.navbar}>
-            <h4><Link href="./portraits"><a>portraits</a></Link></h4>
-            <h4><Link href="./kids"><a>kids</a></Link></h4>
-            <h4><Link href="./preborn"><a>preborn</a></Link></h4>
-            <h4><Link href="./newborn"><a>newborn</a></Link></h4>
-            <h4><Link href="./food"><a>food</a></Link></h4>
-            <h4><Link href="./city"><a>city</a></Link></h4>
-            <h4><Link href="./landscape"><a>landscape</a></Link></h4>
-            <h4><Link href="/"><a>home</a></Link></h4>
-            
+        <div className={styles.entireNavbar}>
+            <div className={styles.navbar}>
+                <h4><Link href="./portraits"><a>portraits</a></Link></h4>
+                <h4><Link href="./kids"><a>kids</a></Link></h4>
+                <h4><Link href="./preborn"><a>preborn</a></Link></h4>
+                <h4><Link href="./newborn"><a>newborn</a></Link></h4>
+                <h4><Link href="./food"><a>food</a></Link></h4>
+                <h4><Link href="./city"><a>city</a></Link></h4>
+                <h4><Link href="./landscape"><a>landscape</a></Link></h4>
+                <h4><Link href="/"><a>home</a></Link></h4>
+            </div>
         </div>
       
         
