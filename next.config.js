2abbbd8152f2e9c1290f01bfc3f0c12a2909efe3
MocanuAlinD <alin_ngt@yahoo.com>
2021-04-21 17:33:43 +0300

    Fix 403 error and deploy

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    images: {
+        loader: 'imgix',
+        path: 'https://cristian-elena.ro/',
+    },
+    trailingSlash: true,
+}
+
+// Path - location of deploy,website  |||  cristianelena.000webhostapp.com
