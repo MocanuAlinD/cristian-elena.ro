d717dee59060bf58f5ccdc15e5a086f0b3f3344d
Mocanu Alin Daniel <alin@Mocanus-MacBook-Air.local>
2022-02-15 15:31:13 +0200

    Try to fix ImageList

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   images: {
     loader: "imgix",
-    // path: 'https://cristian-elena.ro/',
-    path: "http://localhost:3000/" || "https://cristian-elena.ro" || "https://cristian-elena-ro2.vercel.app",
+    // path: 'http://localhost:3000/',
+    path: "https://cristian-elena-ro2.vercel.app",
   },
   trailingSlash: true,
 };
